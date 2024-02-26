7ae0f7fbab72e96e711524e0745086ebcf60b26f
Linus Lundqvist <linus.lundqvist2@elev.ga.ntig.se>
2024-02-26 13:22:59 +0100

    PM skrivande

--- dokumentation/PM.md
+++ dokumentation/PM.md
@@ -33,7 +33,7 @@ Extra arbete egentligen.
 .
 
 # Plannering
-Grundtanken för projektet var en sideview collectathon med ett halloween tema.
+Grundtanken för projektet var en sideview arcade platformer, likt Jump Man (Mario), med ett halloween tema.
 
 
 # Vad
@@ -46,34 +46,34 @@ I spelet ska man kunna springa och hoppa runt för att samla ihop så många pum
 ## Teknik
 För grunden för själva projektet så utgick jag ifrån Jens fantastiska grund benämnd "steg Mario.", detta gav mig en grund som tillåter för en spelkaraktär, även känd som en player, som kan röra sig i sidled och kunna hoppa. 
 
-Tillsamans med denna Player så har vi även platformar som våran karaktär kan manövera sig från och till. 
+Tillsamans med denna Player så har vi även platformar som våran karaktär kan manövera sig utöver. 
 
 Sedan sista grundlägande delen för detta projekt var fiender, vilka kan manövera sig fram och tillbaka på våra platformar.
 
 Utöver denna grund så har jag lagt till en timer för att hålla koll på lång tid spelare får på sig, en score tracker som håller koll på hur många poäng du har och sist men inte minst en game over screen som även visar din slutgiltiga score.
 
 
-# Hur
-hmm
+# Hur?
+
 
 
 # Varför?
-
+SAMMANFATTNING AV VAD JAG VILLE ÅSTAKOMMA OCH HUR JAG ÅSTAKOMMADE DET.
 
 
 # Hur gick det?
 ## Vad gick bra?
-- 
+- I överlag skulle jag beskriva mitt hanterande av kodandet åtminstonde helt okej och i vissa delar faktiskt ganska bra.
 
 ## Vad gick mindre bra?
-- 
+- Att kunna debuga koden och finna roten till olika problem sitter i framfoten till mina största problem med projektet. T.ex. så hakade jag upp mig på att lista ut varför platformarna kände för att inte anse att objekt som låg på toppen av dom var grundade, i slutendan så finns problemet typ fortfarande kvar i vissa fall så jag valde att sido gå problemet istället.
 
 # Lärdomar?
-
+Att kunna debuga kod är viktigt och jag bör förmodligen försöka bli bättre på det.
 
 
 # Uppgiftsreview
 ## Bra?
-
+Uppgiften i överlag var ganska interesant och lärdomsful, vilket gjorde det lätt att arbeta med projektet.
 
 ## Dåligt?
--- src/UserInterface.js
+++ src/UserInterface.js
@@ -14,19 +14,22 @@ export default class UserInterface {
         context.shadowColor = 'black'
         context.textAllign = 'left'
         context.font = `${this.fontSize}px ${this.fontFamily}`
-        context.fillText(`Time: ${(this.Game.gameTime * 0.001).toFixed(1)}`, 20, 40)
+        context.fillText(`Time: ${(60 - this.Game.gameTime * 0.001).toFixed(1)}`, 20, 40)
         context.fillText(`Score: ${this.Game.score}`, 20, 70)
 
         if (this.Game.gameOver) {
             context.font = `50px ${this.fontFamily}`
-            context.fillText('Game Over', this.Game.width / 2 - 150, this.Game.height / 2)
+            context.fillText('Game Over', this.Game.width / 2 - 130, this.Game.height / 2 )
             context.font = `20px ${this.fontFamily}`
-            context.fillText(`Your score was ${this.Game.score} points!`, this.Game.width / 2 - 170, this.Game.height / 2 + 50)
+            context.fillText(`Your score was ${this.Game.score} points!`, this.Game.width / 2 - 110, this.Game.height / 2 + 50)
+            context.fillText(`Press R to restart.`, this.Game.width / 2 - 80, this.Game.height / 2 + 80)
         }
 
         if (this.Game.paused && !this.Game.gameOver) {
             context.font = `20px ${this.fontFamily}`
-            context.fillText('Move with a & d and jump with Space.', this.Game.width / 2 - 170, this.Game.height / 2)
+            context.fillText(`Move with A & D and jump with Space.`, this.Game.width / 2 - 170, this.Game.height / 2)
+            context.fillText(`Gather as many pumpkins as you can!`, this.Game.width / 2 - 170, this.Game.height / 2 + 30)
+            context.fillText(`Press Y to start!`, this.Game.width / 2 - 70, this.Game.height / 2 + 60)
         }
 
         if (this.Game.debug) {
@@ -34,7 +37,7 @@ export default class UserInterface {
             context.font = `${this.fontSize}px ${this.fontFamily}`
             context.fillText(`X: ${(this.Game.Player.x)}`, this.Game.width - 80, 40)
             context.fillText(`Y: ${(this.Game.Player.y)}`, this.Game.width - 80, 70)
-            context.fillText(`Grounded: ${(this.Game.Player.grounded)}`, this.Game.width - 175, 130)
+            context.fillText(`Grounded: ${(this.Game.Player.grounded)}`, this.Game.width - 175, 100)
         }
 
         context.restore()
